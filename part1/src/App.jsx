0078b7de7e983d606b3ecc4461372b3267d7e754
Sathvik PN <sathvikapn@gmail.com>
2025-11-06 09:41:51 +0530

    1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,46 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1> {props.course} </h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p> {props.part} {props.exercise} </p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  const parts = props.parts 
+  const exercises = props.exercises
+
+  return (
+    <>
+      <Part part={parts[0]} exercise={exercises[0]} />
+      <Part part={parts[1]} exercise={exercises[1]} />
+      <Part part={parts[2]} exercise={exercises[2]} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  const numbersArray = props.numbers;
+  const totalSum = numbersArray.reduce((accumulator, currentValue) => {
+    // For each item, add the currentValue to the accumulator (running total)
+    return accumulator + currentValue; 
+  }, 0); // The '0' is the initial value of the accumulator
+
+  return (
+    <>
+      <p>Number of exercises {totalSum}</p>
+    </>
+  );
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -7,19 +50,13 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+  const exercises = [exercises1, exercises2, exercises3]
+  const parts = [part1, part2, part3]
   return (
     <>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content parts={parts} exercises={exercises} />
+      <Total numbers={exercises} />
     </>
   )
 }
